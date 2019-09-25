f0ba124857d7b1a66adab601f944896824f46971
Roel <roel@wheelhouse.be>
2019-09-25 15:31:45 +0200

    KAS-795 Added document title to the document group query

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
 import cors from 'cors';
-
-import { app, query, errorHandler } from 'mu';
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const archiver = require('./utils/archiver');
+import { app, errorHandler } from 'mu';
+import fs from "fs";
+import archiver from "./utils/archiver";
+import bodyParser from "body-parser";
 
 app.use(cors());
 app.use(bodyParser.json({ type: 'application/*+json' }));
 
 app.post('/bundleAllFiles', async (req, res) => {
-  const agenda = JSON.parse(req.body.agenda);
-  const meetingDate = req.body.meetingDate;
-  const files = JSON.parse(req.body.files);
+    const agenda = JSON.parse(req.body.agenda);
+    const meetingDate = req.body.meetingDate;
+    const files = JSON.parse(req.body.files);
 
-  const agendaitems = files.reduce((items, fileObject) => {
-    const foundItem = items.find((item) => item.agendaitem_id == fileObject.agendaitem_id);
-    if (!foundItem) {
-      const item = {
-        agendaitem_id: fileObject.agendaitem_id,
-        agendaitemPrio: fileObject.agendaitemPrio,
-        agendaitemName: `agendapunt_${fileObject.agendaitemPrio}`,
-        filesToDownload: [createUsefullAgendaItem(fileObject)],
-      };
-      items.push(item);
-    } else {
-      foundItem.filesToDownload.push(createUsefullAgendaItem(fileObject));
-    }
-    return items;
-  }, []);
+    const agendaitems = files.reduce((items, fileObject) => {
+        const foundItem = items.find((item) => item.agendaitem_id == fileObject.agendaitem_id);
+        if (!foundItem) {
+            const item = {
+                agendaitem_id: fileObject.agendaitem_id,
+                agendaitemPrio: fileObject.agendaitemPrio,
+                agendaitemName: `agendapunt_${fileObject.agendaitemPrio}`,
+                filesToDownload: [createUsefullAgendaItem(fileObject)],
+            };
+            items.push(item);
+        } else {
+            foundItem.filesToDownload.push(createUsefullAgendaItem(fileObject));
+        }
+        return items;
+    }, []);
 
-  const path = await archiver.archiveFiles(meetingDate, agenda, agendaitems);
-  fs.createReadStream(path).pipe(res);
+    const path = await archiver.archiveFiles(meetingDate, agenda, agendaitems);
+    fs.createReadStream(path).pipe(res);
 });
 
 const createUsefullAgendaItem = (item) => {
-  const name = `${item.numberVR || item.documentVersionName || item.documentVersionId}.${
-    item.extension
-  }`;
-  let download;
-  if (item && item.download) {
-    download = item.download.split('share://')[1];
-  }
+    const name = `${item.numberVR || item.documentVersionName || item.documentVersionId || item.documentTitle}.${
+        item.extension
+    }`;
+    let download;
+    if (item && item.download) {
+        download = item.download.split('share://')[1];
+    }
 
-  return {
-    name,
-    download,
-  };
+    return {
+        name,
+        download,
+    };
 };
 
 app.use(errorHandler);
