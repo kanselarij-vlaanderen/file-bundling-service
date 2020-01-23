ab97fa6ecc8d6089d37946ba01eef80e91559232
Michaël Dierick <michael.dierick@redpencil.io>
2020-01-23 20:02:51 +0100

    Change endpoint name and http method to be more rest api command-like

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import sanitize from 'sanitize-filename';
 app.use(cors());
 app.use(bodyParser.json({ type: 'application/*+json' }));
 
-app.post('/bundleAllFiles', async (req, res) => {
+app.get('/files/archive', async (req, res) => {
   console.log(req.body);
   const files = req.body.data;
   const filesToArchive = files.map((file) => {
