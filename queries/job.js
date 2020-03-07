8473dd180f677932e7cb5a0026bbd85e0ac28b85
Michaël Dierick <michael.dierick@redpencil.io>
2020-03-07 12:28:28 +0100

    Remove unnecessary prefixes

--- queries/job.js
+++ queries/job.js
@@ -102,12 +102,9 @@ async function updateJobStatus (uri, status) {
 
 async function findJobTodo (job) {
   const queryString = `
-  PREFIX cogs: <http://vocab.deri.ie/cogs#>
-  PREFIX prov: <http://www.w3.org/ns/prov#>
   PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
   PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
-  PREFIX dct: <http://purl.org/dc/terms/>
-  PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
+  PREFIX prov: <http://www.w3.org/ns/prov#>
 
   SELECT (?uuid as ?id) ?status ?used WHERE {
       GRAPH ?g {
