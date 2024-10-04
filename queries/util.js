b92d2d29ac7b185ee4768b4b8c13605dcae04070
Sergio Fenoll <sergio@fenoll.be>
2024-10-04 16:57:34 +0200

    chore: add our own retry mechanism for queries
    On boot, sometimes mu-auth isn't ready yet for this service so we should at least retry the query instead of failing.
    
    Maybe there are better ways to have services depend on mu-auth being fully online before starting (some docker depends_on magic perhaps) but that's outside the scope of this branch.

--- queries/util.js
+++ queries/util.js
@@ -13,6 +13,26 @@ const parseSparqlResults = (data) => {
   });
 };
 
+const sparqlQueryWithRetry = async (sparqlMethod, queryString, attempt = 0) => {
+  try {
+    return await sparqlMethod(queryString);
+  } catch (ex) {
+    if (attempt < 5) {
+      // Hardcoded, we want to use the template functionality anyway once it's working instead of making our own solution here
+      attempt += 1;
+      const sleepTime = 2000;
+      console.log(`Query failed, sleeping ${sleepTime} ms before next attempt`);
+      await new Promise(r => setTimeout(r, sleepTime));
+      return await sparqlQueryWithRetry(sparqlMethod, queryString, attempt);
+    } else {
+      console.log(`Failed query:
+        ${queryString}`);
+      throw ex;
+    }
+  }
+}
+
 export {
-  parseSparqlResults
+  parseSparqlResults,
+  sparqlQueryWithRetry,
 };
